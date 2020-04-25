b3688e2dad27abbd35b39fbad6f91816b36c10d2
CMBaines <christopher.mark.baines@gmail.com>
2020-04-25 10:59:04 -0500

    Update SumOfEvenFibonaccisUnderN.py
    precalc sqrt5 and phi

--- SumOfEvenFibonaccisUnderN.py
+++ SumOfEvenFibonaccisUnderN.py
@@ -13,10 +13,14 @@ def SumOfEvenFibonaccisUnderN(N: int):
         m += 3
 
 
+rt5 = sqrt(5)
+
+phi = (1 + rt5)/2
+
 def GenerateNthFibonacci(n: int):
     """Solving the 2nd order linear differential equation F_n = F_n-1 + F_n-2 with F_0 = 0 F_1 = 1
     we can generate the nth term of the Fibonacci sequence"""
-    return (((1+sqrt(5))/2)**n)/sqrt(5)  -  ((1-((1+sqrt(5))/2))**n)/sqrt(5)
+    return phi**n/rt5  -  (1-phi)**n/rt5
 
 res = SumOfEvenFibonaccisUnderN(4000000)
 
