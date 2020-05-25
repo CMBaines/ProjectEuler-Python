b101b0accf43cbb6ed32c644775e9866759b3544
CMBaines <christopher.mark.baines@gmail.com>
2020-05-25 16:32:39 -0500

    Cleaning up inconsistent usage of N and n
    Cleaning up inconsistent usage of N and n as I had been using both

--- ProjectEuler/Problems/Methods/ArithmeticSums/SumOfDivisibleUnderN.py
+++ ProjectEuler/Problems/Methods/ArithmeticSums/SumOfDivisibleUnderN.py
@@ -1,6 +1,6 @@
 from .ArithmeticSum import ArithmeticSum
 
-def SumOfDivisibleUnderN(divisor: int, N: int):
-    """Returns the sum of all numbers divisible by the divisor up to but not including N"""
-    res = ArithmeticSum(N, divisor)
+def SumOfDivisibleUnderN(divisor: int, n: int):
+    """Returns the sum of all numbers divisible by the divisor up to but not including n"""
+    res = ArithmeticSum(n, divisor)
     return res
--- ProjectEuler/Problems/Methods/Fibonacci/SumOfEvenFibonaccisUnderN.py
+++ ProjectEuler/Problems/Methods/Fibonacci/SumOfEvenFibonaccisUnderN.py
@@ -1,13 +1,13 @@
 from .GenerateNthFibonacci import GenerateNthFibonacci
 
-def SumOfEvenFibonaccisUnderN(N: int):
-    """Using the fact that every third term of the Fibonacci sequence is even this returns the sum of the Fibonaccis under N
+def SumOfEvenFibonaccisUnderN(n: int):
+    """Using the fact that every third term of the Fibonacci sequence is even this returns the sum of the Fibonaccis under n
     without generating all the intermidiate odd Fibonacci numbers"""
     sum = 0
     m = 3
     while True:
         term = GenerateNthFibonacci(m)
-        if term >= N:
+        if term >= n:
             return sum
         sum += term
         m += 3
